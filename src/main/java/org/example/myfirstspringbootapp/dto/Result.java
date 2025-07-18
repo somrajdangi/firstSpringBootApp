4e67aa5fd7e3046ebdbf882db6c91b31e9d75298
akshayypi <akshay.agarwal@vertexone.net>
2025-07-18 09:36:57 +0530

    refactoring

---
+++ src/main/java/org/example/myfirstspringbootapp/dto/Result.java
@@ -0,0 +1,15 @@
+package org.example.myfirstspringbootapp.dto;
+
+import lombok.Data;
+/**
+ * Author  AAgarwal
+ * <p>
+ * Date   7/18/2025
+ */
+@Data
+public class Result
+{
+    private int result;
+    private String message;
+    private String status;
+}
---
+++ src/main/java/org/example/myfirstspringbootapp/model/Student.java
@@ -0,0 +1,19 @@
+package org.example.myfirstspringbootapp.model;
+
+import lombok.Data;
+/**
+ * Author  AAgarwal
+ * <p>
+ * Date   7/18/2025
+ */
+@Data
+public class Student
+{
+    private String name;
+    private int age;
+    private String address;
+    private String city;
+    private String state;
+    private String country;
+
+}
